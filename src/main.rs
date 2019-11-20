598ec3b3cba3e08e4aadaf7fa22cb7665440a77a
Sebastian Bolanos <sebasheston@gmail.com>
2019-11-20 08:05:15 -0600

    WIP, string literals and borrowing don't make sense

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
+extern crate config;
+extern crate libnotify;
 extern crate rodio;
-
-use libnotify;
+use config::*;
+use std::collections::HashMap;
+use std::env;
 use std::io::BufReader;
 use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 
-const FIVE_MINUTES: u64 = 5;
-const TWENTY_MINUTES: u64 = 20;
-const BOJACK: &'static str = "/home/sebas/repos/programs/romodoro/src/bojack.mp3";
-const BOWL: &'static str = "/home/sebas/repos/programs/romodoro/src/bowl.mp3";
-
 #[derive(Debug, Copy, Clone)]
 struct TransitionData {
     duration: Duration,
-    message: &'static str,
+    message: &'a str,
     sound: &'static str,
     urgency: libnotify::Urgency,
 }
@@ -35,40 +33,102 @@ fn playback(filename: &'static str) {
 
 fn process_state(data: TransitionData) {
     let n = libnotify::Notification::new(
-        &format!("{} minutes", data.duration.as_secs()),
+        &format!("{} minutes done", data.duration.as_secs() / 60),
         Some(data.message),
         None,
     );
     thread::spawn(move || playback(data.sound));
     n.set_urgency(data.urgency);
     n.show().unwrap();
-    sleep(data.duration);
 }
 
-fn main() {
-    libnotify::init("romodor").unwrap();
+struct ConfigTable {
+    sound: String,
+    duration: Duration,
+}
+
+fn extract_table(table: HashMap<String, Value>) -> ConfigTable {
+    let sound : String;
+    match table.get("sound") {
+        Some(entry) => {
+            sound = entry.to_string()
+            }
+        None => {sound = "".to_string()}
+    }
+
+    let duration: u64;
+    match table.get("duration") {
+        Some(entry) => {
+            duration = entry.to_string().parse::<u64>().expect("Not a number")
+        }
+        None => {duration = 0}
+    }
+    return ConfigTable {
+        sound: sound,
+        duration: Duration::from_secs(duration),
+    }
+}
+
+fn run(settings: config::Config) {
     let mut current_state = State::BeginWork;
+
+    let long_break = settings.get_table("long_break").expect("no long_break table");
+    let long_break = extract_table(long_break);
+
+    let short_break = settings.get_table("short_break").expect("no short_break table");
+    let short_break = extract_table(short_break);
+
+    let begin_work = settings.get_table("begin_work").expect("no begin_work table");
+    let begin_work = extract_table(begin_work);
+
+
     loop {
         match current_state {
             State::BeginWork => {
                 process_state(TransitionData {
-                    duration: Duration::from_secs(TWENTY_MINUTES),
-                    message: "Begin work",
-                    sound: BOJACK,
                     urgency: libnotify::Urgency::Critical,
+                    message: "Begin work",
+                    sound: begin_work.sound.as_ref(),
+                    duration: begin_work.duration
                 });
+                sleep(begin_work.duration);
                 current_state = State::ShortBreak;
-            },
+            }
             State::ShortBreak => {
                 process_state(TransitionData {
-                    duration: Duration::from_secs(FIVE_MINUTES),
-                    message: "Take a break 😁",
-                    sound: BOWL,
                     urgency: libnotify::Urgency::Low,
+                    message: "Take a break 😁",
+                    sound: &short_break.sound,
+                    duration: short_break.duration
                 });
+                sleep(short_break.duration);
+                current_state = State::LongBreak;
+            }
+            State::LongBreak => {
+                process_state(TransitionData {
+                    urgency: libnotify::Urgency::Low,
+                    message: "Take a looooong break 😁",
+                    sound: &long_break.sound,
+                    duration: long_break.duration
+                });
+                sleep(long_break.duration);
                 current_state = State::BeginWork;
             }
-            State::LongBreak => println!("hi"),
         }
     }
+}
+
+fn main() {
+    libnotify::init("romodoro").unwrap();
+
+    let mut config_path: String = env::var("XDG_CONFIG_HOME").unwrap();
+    config_path.push_str("/romodoro");
+    let mut settings = Config::default();
+    settings
+        .merge(config::File::with_name(config_path.as_ref()))
+        .unwrap()
+        .merge(config::Environment::with_prefix("ROMODORO"))
+        .unwrap();
+
+    run(settings);
 }
