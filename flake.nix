8ab91962428d329ecd595451a79e99e6ccbcc379
Sebastian Bolanos <sebasheston@protonmail.com>
2021-05-09 18:47:59 +0100

    Init flake.nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "my project description";
+
+  inputs.flake-utils.url = "github:sbc64/romodoro";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
